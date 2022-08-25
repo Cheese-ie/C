0f3988a9478de571e153724f2115e11346991103
Cheese-ie <108739103+Cheese-ie@users.noreply.github.com>
2022-08-25 13:53:58 +0800

    练习

---
+++ 4-3练习.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main(void)
+{
+	printf("ÇëÊäÈëÒ»¸ö¸¡µãÊý:");
+	float f;
+	scanf("%f", &f);
+	printf("\na.The input is %.1f or %.1e", f, f);
+	printf("\nb.The input is %+.3f or %.3E", f, f);
+
+	return 0;
+}
