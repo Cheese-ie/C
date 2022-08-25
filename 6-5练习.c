38fbf16129fda1e7470aa4c03ae15896adca3cb8
Cheese-ie <108739103+Cheese-ie@users.noreply.github.com>
2022-08-25 20:28:02 +0800

    练习

--- 6-5练习.c
+++ 6-5练习.c
@@ -1,20 +1,20 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int main(void)
-{	
-	char x,y,z;
-	printf("Enter a capital letter:");
-	for (scanf("%c", &x),z='A'; x >= 'A'; x--, z++)
-	{
-		for (y = 'A'; y <= x; y++)
-			printf(" ");
-		for (y = 'A'; y < z; y++)
-			printf("%c", y);
-		for (y=z; y>='A'; y--)
-			printf("%c", y);
-		printf("\n");
-		
-	}
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main(void)
+//{	
+//	char x,y,z;
+//	printf("Enter a capital letter:");
+//	for (scanf("%c", &x),z='A'; x >= 'A'; x--, z++)
+//	{
+//		for (y = 'A'; y <= x; y++)
+//			printf(" ");
+//		for (y = 'A'; y < z; y++)
+//			printf("%c", y);
+//		for (y=z; y>='A'; y--)
+//			printf("%c", y);
+//		printf("\n");
+//		
+//	}
+//	return 0;
+//}
