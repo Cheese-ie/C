6ff8b8bed5e2eb4e76af1765c7b6778a39d8734b
Cheese-ie <108739103+Cheese-ie@users.noreply.github.com>
2022-08-25 13:49:28 +0800

    Update 6-3练习.c

--- 6-3练习.c
+++ 6-3练习.c
@@ -1,14 +1,14 @@
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	int i,x;
-//	char j;
-//	for (i=1; i <=6; i++)
-//	{
-//		for (j = 'F',x=1; x<=i; j--,x++)
-//			printf("%c", j);
-//		printf("\n");
-//	}
-//	return 0;
-//}
+#include <stdio.h>
+
+int main(void)
+{
+	int i,x;
+	char j;
+	for (i=1; i <=6; i++)
+	{
+		for (j = 'F',x=1; x<=i; j--,x++)
+			printf("%c", j);
+		printf("\n");
+	}
+	return 0;
+}
